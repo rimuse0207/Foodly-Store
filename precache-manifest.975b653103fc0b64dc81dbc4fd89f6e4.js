8f1576eafc5df01a702bbd764cc9929791e24418
sleny <rimuse0207@naver.com>
2020-09-17 10:44:41 +0900

    Updates

---
+++ precache-manifest.975b653103fc0b64dc81dbc4fd89f6e4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaf70dfd16fb73d65631ef4602df1e5e",
+    "url": "/Foodly-Store/index.html"
+  },
+  {
+    "revision": "8fa452c95c27ae1db32a",
+    "url": "/Foodly-Store/static/css/main.b2124551.chunk.css"
+  },
+  {
+    "revision": "39bd5ae30ad153c1f3a0",
+    "url": "/Foodly-Store/static/js/2.9ef708d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Foodly-Store/static/js/2.9ef708d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fa452c95c27ae1db32a",
+    "url": "/Foodly-Store/static/js/main.83267489.chunk.js"
+  },
+  {
+    "revision": "4fd5c4d3e4dfa9f1a87c",
+    "url": "/Foodly-Store/static/js/runtime-main.7615d4e5.js"
+  },
+  {
+    "revision": "c5770c6994dbc7471c92d708951922f6",
+    "url": "/Foodly-Store/static/media/Basil_grande.c5770c69.jpeg"
+  },
+  {
+    "revision": "ddc90681e2807592f20479a9c24e5d08",
+    "url": "/Foodly-Store/static/media/Brandywine-tomatoes-front_grande.ddc90681.jpeg"
+  },
+  {
+    "revision": "e03a3183d111bb3bef7a16ab5da815ad",
+    "url": "/Foodly-Store/static/media/dada.e03a3183.jpg"
+  },
+  {
+    "revision": "a805fc8ea30b7cd430a8a38a7b29ec6a",
+    "url": "/Foodly-Store/static/media/img_homepage_slide_1_99ab6459-79aa-49be-89c7-fb4cd2ac027f_2000x.a805fc8e.jpg"
+  },
+  {
+    "revision": "8027103dda84dd1fa4f87b5fc6ef2c72",
+    "url": "/Foodly-Store/static/media/img_homepage_slide_2_1e0ca3da-09bc-46b3-a58e-952f99df5a5d_2000x.8027103d.jpg"
+  },
+  {
+    "revision": "211705ae097a175eec3edd21dcc38ea6",
+    "url": "/Foodly-Store/static/media/img_homepage_slide_3_2d5607e9-6072-431a-9965-9e0421c370c6_2000x.211705ae.jpg"
+  },
+  {
+    "revision": "385ddfddf3151fdd2799fb5e63d82e4c",
+    "url": "/Foodly-Store/static/media/img_homepage_special_product_600x.385ddfdd.png"
+  },
+  {
+    "revision": "907cc8b220e298e50fab973a1d6c0a66",
+    "url": "/Foodly-Store/static/media/raspberries_ed450429-f3d8-4757-9507-f18101c2c057_grande.907cc8b2.jpeg"
+  }
+]);
