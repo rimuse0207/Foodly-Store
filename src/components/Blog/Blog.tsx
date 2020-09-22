a60f3c37b252620a705ca4b6820a65eed456c158
sleny <rimuse0207@naver.com>
2020-09-22 13:55:30 +0900

    typescript add

--- src/components/Blog/Blog.tsx
+++ src/components/Blog/Blog.tsx
@@ -1,56 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../Css/Blog.css";
+import "../../conf.d.ts";
+type Blogs = {
+  id: number;
+  img: string;
+  title: string;
+  date: string;
+  description: string;
+  writer: string;
+};
+declare function require(path: string);
 const Blog: React.FC = () => {
-  const FirstImage = require("../../asset/blog/blog_news_02_large.jpg");
-  const SecondImage = require("../../asset/blog/blog_news_01_large.jpg");
-  const ThirdImage = require("../../asset/blog/article_01_large.jpg");
+  const [Blogs, setBlogs] = useState<Blogs[]>([
+    {
+      id: 1,
+      img: "blog/blog_news_02_large.jpg",
+      title: "What We Got This Fall",
+      date: "February 23, 2016",
+      description:
+        "Happy harvesting time with huge expectations for the next season.Harvest is the process of gathering ripe crops from the fields.Reaping is the cutting of grain or pulse for harvest, typicallyusing a scythe, sickle, or reaper.",
+      writer: "by Ann Reed",
+    },
+    {
+      id: 2,
+      img: "blog/blog_news_01_large.jpg",
+      title: "Apple Harvest 2015",
+      date: "February 23, 2016",
+      description:
+        "In the Foodly Kitchen, we’re partial to McIntosh and Granny Smithfor baking. When the softer McIntosh mixes with the more toothsomeGranny Smith, presto! You’ve got yourself the perfect apple pie.",
+      writer: "by Ann Reed",
+    },
+    {
+      id: 3,
+      img: "blog/article_01_large.jpg",
+      title: "Raspberries Harvest 2015",
+      date: "February 23, 2016",
+      description:
+        " We started growing raspberries in 90’s on our family farm nearCupar. Our cultivar has exeptional sweet taste with astringentflavor. These individually gathered berries are a wonderful taste ofsummer in your everyday meal. Raspberries are perfect in smoothies,baking, on pancakes or just plain old snacking out of the bag!Enjoy!",
+      writer: "by Ann Reed",
+    },
+  ]);
+
   return (
     <div className="Blogs">
       <h2>BLOG</h2>
       <em>with love from the bottom of Foodlyheart</em>
-      <div className="Blog">
-        <div>
-          <img src={FirstImage}></img>
-          <strong>What We Got This Fall</strong>
-          <span>February 23, 2016</span>
-          <em>
-            Happy harvesting time with huge expectations for the next season.
-            Harvest is the process of gathering ripe crops from the fields.
-            Reaping is the cutting of grain or pulse for harvest, typically
-            using a scythe, sickle, or reaper.
-          </em>
-          <span>by Ann Reed</span>
-        </div>
-      </div>
-      <div className="Blog">
-        <div>
-          <img src={SecondImage}></img>
-          <strong>Apple Harvest 2015</strong>
-          <span>February 23, 2016</span>
-          <em>
-            In the Foodly Kitchen, we’re partial to McIntosh and Granny Smith
-            for baking. When the softer McIntosh mixes with the more toothsome
-            Granny Smith, presto! You’ve got yourself the perfect apple pie.
-          </em>
-          <span>by Ann Reed</span>
-        </div>
-      </div>
-      <div className="Blog">
-        <div>
-          <img src={ThirdImage}></img>
-          <strong>Raspberries Harvest 2015</strong>
-          <span>February 23, 2016</span>
-          <em>
-            We started growing raspberries in 90’s on our family farm near
-            Cupar. Our cultivar has exeptional sweet taste with astringent
-            flavor. These individually gathered berries are a wonderful taste of
-            summer in your everyday meal. Raspberries are perfect in smoothies,
-            baking, on pancakes or just plain old snacking out of the bag!
-            Enjoy!
-          </em>
-          <span>by Ann Reed</span>
-        </div>
-      </div>
+      {Blogs.map((list) => {
+        return (
+          <div className="Blog" key={list.id}>
+            <div>
+              <img src={require(`../../asset/${list.img}`)}></img>
+              <strong>{list.title}</strong>
+              <span>{list.date}</span>
+              <em>{list.description}</em>
+              <span>{list.writer}</span>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../Css/Products.css";
-
+type Prodcutss = {
+  id: number;
+  img: string;
+  title: string;
+};
 const Products: React.FC = () => {
+  const [productss, setProductss] = useState([
+    {
+      id: 1,
+      img: "collection_01_all_large (1).jpeg",
+      title: "All products",
+    },
+    {
+      id: 2,
+      img: "collection_02_fresh_small.png",
+      title: "Fresh",
+    },
+    {
+      id: 3,
+      img: "collection_03_frozen_small.png",
+      title: "Frozen",
+    },
+    {
+      id: 4,
+      img: "collection_04_dried_small.png",
+      title: "Dried",
+    },
+    {
+      id: 5,
+      img: "collection_05_liquid_small.png",
+      title: "Liquid",
+    },
+    {
+      id: 6,
+      img: "collection_06_sale_large.png",
+      title: "Sale",
+    },
+    {
+      id: 7,
+      img: "collection_07_specials_large.png",
+      title: "Specials",
+    },
+    {
+      id: 8,
+      img: "collection_08_cooked_large.png",
+      title: "Cooked",
+    },
+  ]);
   const All_products = require("../../asset/Products/collection_01_all_large (1).jpeg");
   const Fresh = require("../../asset/Products/collection_02_fresh_small.png");
   const Frozen = require("../../asset/Products/collection_03_frozen_small.png");
@@ -15,71 +61,19 @@ const Products: React.FC = () => {
     <div className="Products">
       <h2>PRODUCT COLLECTIONS</h2>
       <ul>
-        <li>
-          <a href="">
-            <div>
-              <img src={All_products}></img>
-            </div>
-            <span>All products</span>
-          </a>
-          <div className="colorChangeHover"></div>
-        </li>
-        <li>
-          <a href="">
-            <div>
-              <img src={Fresh}></img>
-            </div>
-            <span>Fresh</span>
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <div>
-              <img src={Frozen}></img>
-            </div>
-            <span>Frozen</span>
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <div>
-              <img src={Dried}></img>
-            </div>
-            <span>Dried</span>
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <div>
-              <img src={Liquid}></img>
-            </div>
-            <span>Liquid</span>
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <div>
-              <img src={Sale}></img>
-            </div>
-            <span>Sale</span>
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <div>
-              <img src={Specials}></img>
-            </div>
-            <span>Specials</span>
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <div>
-              <img src={Cooked}></img>
-            </div>
-            <span>Cooked</span>
-          </a>
-        </li>
+        {productss.map((list) => {
+          return (
+            <li key={list.id}>
+              <a href="">
+                <div>
+                  <img src={require(`../../asset/Products/${list.img}`)}></img>
+                </div>
+                <span>{list.title}</span>
+              </a>
+              <div className="colorChangeHover"></div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
